5965f00a2185ca24ddac1e4b8e3af4a212d82c64
No-YE <nye7181@gmail.com>
2019-04-23 10:34:46 +0900

    logger 설정, http2 설정 완료

--- src/bin/www.ts
+++ src/bin/www.ts
@@ -1,6 +1,19 @@
+import { createServer } from 'spdy'
 import { config } from 'dotenv'
+import { readFileSync } from 'fs'
 import app from '../app'
 
 config()
 
-app.listen(3000, () => console.log('start'))
+const keyPath = process.env.KEY_PATH as string
+const certPath = process.env.CERT_PATH as string
+
+const options = {
+  key: readFileSync(keyPath),
+  cert: readFileSync(certPath),
+  protocol: ['h2']
+}
+
+const PORT = process.env.PORT || 3000
+
+createServer(options, app).listen(PORT, () => console.log('start'))
