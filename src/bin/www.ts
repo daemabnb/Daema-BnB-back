0251d27c9c7e9e033a05baffc4bb98d963be35f4
No-YE <nye7181@gmail.com>
2019-05-21 11:00:27 +0900

    http2에서 http1로 변경

--- src/bin/www.ts
+++ src/bin/www.ts
@@ -1,14 +1,6 @@
-import { createServer } from 'spdy'
-import { readFileSync } from 'fs'
+import { createServer } from 'http'
 import app from '../app'
-import { keyPath, certPath } from '../config'
-
-const spdyOptions = {
-  key: readFileSync(keyPath),
-  cert: readFileSync(certPath),
-  protocol: ['h2']
-}
 
 const PORT = process.env.PORT || 3000
 
-createServer(spdyOptions, app).listen(PORT, () => console.log('start'))
+createServer(app).listen(PORT, () => console.log('start'))
