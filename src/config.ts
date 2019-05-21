b2820d9ee1a3aed842dde99d3a14df2f13d53046
No-YE <nye7181@gmail.com>
2019-05-21 16:46:10 +0900

    facebook callback url을 config 파일로 변경

--- src/config.ts
+++ src/config.ts
@@ -10,6 +10,7 @@ const emailId = process.env.EMAIL_ID as string
 const emailPw = process.env.EMAIL_PW as string
 const clientID = process.env.FACEBOOK_CLIENT_ID as string
 const clientSecret = process.env.FACEBOOK_CLIENT_SECRET as string
+const facebookCallback = process.env.FACEBOOK_CALLBACK as string
 const accessKey = process.env.ACCESS_KEY as string
 const secretAccessKey = process.env.SECRET_ACCESS_KEY as string
 const slackUrl = process.env.SLACK_URL as string
@@ -17,4 +18,4 @@ const redisHost = process.env.REDIS_HOST as string
 const redisPort = parseInt(process.env.REDIS_PORT as string, 10)
 
 export { keyPath, certPath, mongoUri, jwtSecret, emailId,
-  emailPw, clientID, clientSecret, accessKey, secretAccessKey, slackUrl, redisHost, redisPort }
+  emailPw, clientID, clientSecret, facebookCallback, accessKey, secretAccessKey, slackUrl, redisHost, redisPort }
