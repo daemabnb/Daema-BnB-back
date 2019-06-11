ad9e85ca4bf99e87ed42c0fa1959fd1d67c59329
No-YE <nye7181@gmail.com>
2019-06-11 18:47:36 +0900

    passport 삭제

--- src/routes/user/ctrl.ts
+++ src/routes/user/ctrl.ts
@@ -2,9 +2,8 @@ import { Request, Response, NextFunction, RequestHandler } from 'express'
 import DB from '../../model/index'
 import mailer from '../../util/mailer'
 import * as redis from '../../util/redis'
-import passport from '../../util/passport'
+import { getRequest } from '../../util/request'
 import { createToken } from '../../util/jwt'
-import logger from '../../util/logger'
 
 const db: DB = new DB()
 
@@ -22,35 +21,8 @@ const postAuthemail: RequestHandler = async (req: Request, res: Response, next: 
   }
 }
 
-const getSigninFacebook: RequestHandler = passport.authenticate('facebook', {
-  session: false,
-  scope: ['public_profile', 'user_link']
-})
+const getSigninFacebook: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
 
-const signinFacebookCallback: RequestHandler = (req: Request, res: Response, next: NextFunction) => {
-  passport.authenticate('facebook', {
-    session: false
-  }, (err, user, info) => {
-    logger.info(JSON.stringify(info))
-
-    if (err) {
-      next(err)
-    }
-
-    const { id, displayName, profileUrl, email, isAdmin } = info
-    const token = createToken(id, displayName, profileUrl, email)
-
-    if (user === true) {
-      return res.status(200).json({
-        nickname: displayName,
-        isAdmin,
-        token
-      }).end()
-    }
-
-    res.status(201).json({ token }).end()
-
-  })(req, res, next)
 }
 
 const postSignup: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
@@ -81,4 +53,4 @@ const postSignup: RequestHandler = async (req: Request, res: Response, next: Nex
   }
 }
 
-export { postAuthemail, getSigninFacebook, signinFacebookCallback, postSignup }
+export { postAuthemail, getSigninFacebook, postSignup }
--- src/routes/user/index.ts
+++ src/routes/user/index.ts
@@ -6,7 +6,6 @@ const userRouter: Router = Router()
 
 userRouter.post('/authemail', ctrl.postAuthemail)
   .get('/signin/facebook', ctrl.getSigninFacebook)
-  .get('/signin/facebook/callback', ctrl.signinFacebookCallback)
   .post('/signup', verifyTokenWithoutEmail, ctrl.postSignup)
 
 export default userRouter
