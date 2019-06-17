c19c5aa4cf1b9e57320940dd336539057ad9be53
No-YE <nye7181@gmail.com>
2019-06-17 22:48:33 +0900

    deploy 전 none 이미지 삭제

--- script/deploy.sh
+++ script/deploy.sh
@@ -9,6 +9,9 @@ fi
 if [ "$(docker images | grep "daemabnb-api")" != "" ]; then
   docker rmi -f $(docker images | grep "daemabnb-api")
 fi
+if [ "$(docker images -f "dangling=true" -q)" != ""]; then
+  docker rmi $(docker images -f "dangling=true" -q)
+fi
 
 sudo docker build -t daemabnb-api .
 sudo docker run -p 3333:3000 -d daemabnb-api
